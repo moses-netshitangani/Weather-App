07a28c4b2eb07cb3b790279d01cb9d6e56801bd0
Moses <netshitanganimoses@gmail.com>
2020-05-04 11:01:05 +0200

    Now showing both the temp and weather type

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,9 +1,11 @@
-// Get temp h1
-let change = t => {
+// Update the DOM
+let change = (t,w) => {
     temp_heading = document.getElementById("temp");
+    type_heading = document.getElementById("type");
     t = t - 273.15;
     t = Math.round(t);
     temp_heading.innerHTML = t;
+    type_heading.innerHTML = w[0].description;
 }
 
 // Coordinates
@@ -14,15 +16,14 @@ let obtain = () => {
     navigator.geolocation.getCurrentPosition(position => {
         lat = position.coords.latitude;
         longi = position.coords.longitude;
-        lat = Math.round(lat);
-        longi = Math.round(longi);
+        lat = lat.toFixed(4);
+        longi = longi.toFixed(4);
         
         // Consume the API
         let apikey = "1b03107903aa41c9654fefe8a6775b7a";
         fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${longi}&appid=${apikey}`)
             .then(response => response = response.json())
-            // .then(data => change(data.main.temp))
-            .then(data => console.log(data))
+            .then(data => change(data.main.temp, data.weather))
             .catch(err => console.log(err));
     });
 }
